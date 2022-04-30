319e5280ff25a3009be4c344c90f6f44881951b0
Damitha Perera <damitha.perera@keeneye.solutions>
2022-04-30 21:37:31 +0530

    DB connected

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-
+const database = require('./database/db');
 const app = express();
 
+const port = 3000;
+database()
 
-app.listen(3000, () =>
+app.listen(port, () =>
     console.log('Example app listening on port 3000!'),
 );
 
