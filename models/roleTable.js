5fc968d3f4ed78022197b829dc5ced6849b01c6d
Damitha Perera <damitha.perera@keeneye.solutions>
2022-05-01 00:50:17 +0530

    issue fixed

--- models/roleTable.js
+++ models/roleTable.js
@@ -7,11 +7,13 @@ const createRoleTable = async () => {
         type VARCHAR(10) NOT NULL
     )`;
 
-    const type1 = `INSERT OR REPLACE INTO roles(id, type) VALUES (1, 'CUSTOMER')`;
-    const type2 = `INSERT OR REPLACE INTO roles(id, type) VALUES (2, 'ADMIN')`;
+    const type1 = `INSERT INTO roles(id, type) SELECT 1, 'CUSTOMER' WHERE NOT EXISTS (SELECT 1 FROM roles WHERE 
+                    type = 'CUSTOMER')`;
+    const type2 = `INSERT INTO roles(id, type) SELECT 2, 'ADMIN' WHERE NOT EXISTS (SELECT 2 FROM roles WHERE 
+                    type = 'ADMIN')`;
 
     try {
-        await connection.run(roleTable);
+        connection.run(roleTable);
         connection.run(type1);
         connection.run(type2);
     } catch (error) {
